efd4d31614dad58606169c0922f7433379d6f602
Charles <makosakarol@gmail.com>
2018-11-19 20:23:27 +0100

    painter improvement - split face to 4 faces

--- painter.py
+++ painter.py
@@ -3,13 +3,18 @@ class Painter(object):
         self.scene = scene
 
     def draw(self):
-        faces = []
-        for shape in self.scene.shapes:
-            for face in shape.faces:
-                faces.append(face)
+        faces = self.split_faces()
+        print(faces)
         faces.sort(key=self.distance_from_camera, reverse=True)
         for face in faces:
             face.draw(self.scene)
 
     def distance_from_camera(self, face):
         return face.distance_from_camera(self.scene)
+
+    def split_faces(self):
+        split_faces = []
+        for shape in self.scene.shapes:
+            for face in shape.faces:
+                split_faces.extend(face.split())
+        return split_faces
